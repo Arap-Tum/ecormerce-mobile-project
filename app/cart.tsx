cde145d2b744af1a2e2ee1fe6d1edf9ad8356fb5
Your Name <you@example.com>
2025-08-29 10:16:54 +0300

    set up exporouter

---
+++ app/cart.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function CartScrean() {
+    return(
+        <View>
+            <Text style={{fontSize: 30}}>Cart Screen </Text>
+        </View>
+    )
+}
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+
+export default function HomeScrean() {
+    return(
+        <View>
+            <Text style={{fontSize: 30}}>Home Screen </Text>
+        </View>
+    )
+}
